ac4d668a2136327450acd9452cd64b176833bb49
Torcoroma <torcoromaarias@gmail.com>
2023-01-15 12:36:32 -0500

    Add formik funcionado

--- src/components/FormLogIn.jsx
+++ src/components/FormLogIn.jsx
@@ -3,66 +3,72 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { ContainerFormik } from "../assets/styled/FormLogin";
 import { Link } from "react-router-dom";
 export const FormLogIn = () => {
+  const [texSend, setTexSend] = useState(false);
   return (
     <>
+      <h2>Inicia sesión</h2>
       <Formik
-        initialValue={{
-          name: "",
-          email: "",
+        initialValues={{
+          nombre: "",
+          correo: "",
         }}
         validate={(valores) => {
           let errores = {};
-          if (!errores.name) {
-            errores.name = "Porfavor ingresa un nombre";
-          } else if (!/^[a-zA-ZA-y\s]{1,40}$/.test(valores.name)) {
-            errores.name = "El nombre solo puede tener letras y espacio";
+          // validacion name
+          if (!valores.nombre) {
+            errores.nombre = "Porfavor ingresa un nombre";
+          } else if (!/^[a-zA-ZA-y\s]{1,40}$/.test(valores.nombre)) {
+            errores.nombre = "El nombre solo puede tener letras y espacio";
           }
 
           // validacion correo
-          if (!errores.email) {
-            errores.email = "Porfavor ingresa un nombre";
+          if (!valores.correo) {
+            errores.correo = "ingrese el correo";
           } else if (
             !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
-              valores.email
+              valores.correo
             )
           ) {
-            errores.email = "El nombre solo puede tener letras y espacio";
+            errores.correo = "El correo es incorrecto";
           }
           return errores;
         }}
         onSubmit={(valores, { resetForm }) => {
           resetForm();
-          console.log("fout");
-          setFomEnvia(true);
 
-          setTimeout(() => setFomEnvia(false), 5000);
+          setTexSend(true);
+
+          setTimeout(() => setTexSend(false), 5000);
         }}
       >
         {({ errors }) => (
-          <ContainerFormik>
-            <Form className="form ">
+          <Form>
+            <div className="">
               <label>nombre</label>
-              <Field type="text" id="name" name="name" placeholder="Nombre" />
+              <Field
+                type="text"
+                id="nombre"
+                name="nombre"
+                placeholder="Nombre"
+              />
               <ErrorMessage
-                name="name"
+                name="nombre"
                 component={() => (
-                  <div className="text-oragen">{errors.name}</div>
+                  <div className="text-oragen">{errors.nombre}</div>
                 )}
               />
-
-              <label>Correo</label>
-              <Field type="text" id="email" name="email" placeholder="@" />
+            </div>
+            <div className="login__container--form">
+              <label>correo</label>
+              <Field type="email" id="correo" name="correo" />
               <ErrorMessage
-                name="email"
-                component={() => (
-                  <div className="text-oragen">{errors.email}</div>
-                )}
+                name="correo"
+                component={() => <div className="">{errors.correo}</div>}
               />
-
-              <button type="submit"> Enviar</button>
-              {/* {formEnvia && <p>Formularia enviado con exito</p>} */}
-            </Form>
-          </ContainerFormik>
+            </div>
+            <button type="submit"> Enviar</button>
+            {texSend && <p>Formularia enviado con exito</p>}
+          </Form>
         )}
       </Formik>
     </>
